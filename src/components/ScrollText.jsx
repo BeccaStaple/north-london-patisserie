2896ad4e4df4817d1246f6a11eac26a203129725
BeccaStaple <56489791+BeccaStaple@users.noreply.github.com>
2020-11-13 17:01:21 +0000

    added animation

--- src/components/ScrollText.jsx
+++ src/components/ScrollText.jsx
@@ -1,9 +1,18 @@
-import React from "react" 
+import React from "react"
 
 export default function ScrollingText() {
-    return(
-        <p>
-            Cakes  -  Tarts  -  Brownies  -  Cookies  -  Biscuits  -  
+    return (
+        <div className="scrolling-sec">
+            <p className="scrolling-text">
+                <span className="scroll-item">Cakes</span> <span className="scroll-item">Tarts</span> 
+                <span className="scroll-item">Brownies</span>
+                <span className="scroll-item">Cookies</span> 
+                <span className="scroll-item">Biscuits</span> <span className="scroll-item">Triffle</span> 
+                <span className="scroll-item">Bread</span>
+                      
         </p>
+       
+        </div>
+
     )
 }
